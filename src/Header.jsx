6dc81d31fdee45c57c6548ec2d294797d53d58ff
cyberace313 <zafer.arifi290@gmail.com>
2023-12-11 17:06:31 +0430

    Intial commit

---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { ReactDOM } from "react";
+
+function Heading() {
+  return (
+    <div className="heading">
+      <h1>Blog Website</h1>
+    </div>
+  );
+}
+
+export default Heading;
